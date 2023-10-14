aeb1d2490ca7080de5ae79be05be36e54a8cbdc4
altananay <altananay@outlook.com>
2023-10-15 02:22:31 +0300

    java singleton pattern modified for thread safe and clonable.

--- java/singleton/src/main/java/org/designpatterns/CustomerManager.java
+++ java/singleton/src/main/java/org/designpatterns/CustomerManager.java
@@ -1,15 +1,31 @@
 package org.designpatterns;
 
 public class CustomerManager {
-  private static CustomerManager customerManager;
+  private static volatile CustomerManager customerManager;
 
   private CustomerManager() {}
 
   public static CustomerManager createAsSingleton() {
-    return (customerManager == null) ? customerManager = new CustomerManager() : customerManager;
+    if (customerManager == null)
+    {
+      synchronized (CustomerManager.class)
+      {
+        if (customerManager == null)
+        {
+          customerManager = new CustomerManager();
+        }
+      }
+    }
+
+    return customerManager;
   }
 
   public void Save() {
     System.out.println("Saved.");
+  }
+
+  @Override
+  protected Object clone() throws CloneNotSupportedException {
+    throw new CloneNotSupportedException();
   }
 }
